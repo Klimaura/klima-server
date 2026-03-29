4af6a2a7fd8f11bf4fb3969f4315e01f3529cc99
Klimaura <calathea.d3sign@gmail.com>
2026-03-29 12:52:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('KLIMA server online'));
 
-// usa solo la porta fornita da Railway
+// IMPORTANTISSIMO: usare SOLO process.env.PORT
 const PORT = process.env.PORT;
 if (!PORT) {
-    console.error("Error: PORT non definita in process.env");
-    process.exit(1); // chiude subito se non c'è porta
+    console.error("Error: process.env.PORT non definita!");
+    process.exit(1);
 }
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
